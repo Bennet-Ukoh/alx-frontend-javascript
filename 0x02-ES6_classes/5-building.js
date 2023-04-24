f4c7c867290a61cd9d558bdfa1455f8adebde4a0
Bennet-Ukoh <bennetukoh@gmail.com>
2023-04-24 12:20:35 +0000

    5-building.js eslint errors fixed

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -7,11 +7,12 @@ export default class Building {
     this._sqft = sqft;
   }
 
-  get sqft() {
-    return this._sqft;
-  }
-
   evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+    if (this.constructor === Building) {
+      throw new Error('Abstract class "Building" cannot be instantiated directly.');
+    }
+    if (typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 }
