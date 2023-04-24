cddfd8243e72219d9b179a4cf3b5e41b3402c0d7
Bennet-Ukoh <bennetukoh@gmail.com>
2023-04-24 09:57:18 +0000

    5-building.js

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,22 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw TypeError('sqft must be a number');
+    }
+
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    if (this.constructor === Building) {
+      throw new Error('Abstract class "Building" cannot be instantiated directly.');
+    }
+    if (typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+}
