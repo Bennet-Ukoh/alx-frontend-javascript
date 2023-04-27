4cff654b729700bf9d58e00ae81146ec52edfaca
Bennet-Ukoh <bennetukoh@gmail.com>
2023-04-27 11:01:39 +0000

    printing teachers

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,3 +1,9 @@
-interface Directors extends Teacher {
-  numberOfReports: number;
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+function printTeacher(firstName: string, lastName: string): string {
+  const initial = firstName.charAt(0).toUpperCase();
+  const fullLastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+  return `${initial}. ${fullLastName}`;
 }
