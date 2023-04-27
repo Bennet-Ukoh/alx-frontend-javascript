26a9c522b969f1b0099c7c06601e397fea29a44b
Bennet-Ukoh <bennetukoh@gmail.com>
2023-04-27 12:15:31 +0000

    Creating functions spacific to employees file added

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,47 +1,11 @@
-interface DirectorInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workDirectorTasks(): string;
+function isDirector(employee: DirectorInterface | TeacherInterface): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
 }
 
-interface TeacherInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workTeacherTasks(): string;
-}
-
-class Director implements DirectorInterface {
-  workFromHome(): string {
-    return 'Working from home';
-  }
-
-  getCoffeeBreak(): string {
-    return 'Getting a coffee break';
-  }
-
-  workDirectorTasks(): string {
-    return 'Getting to director tasks';
-  }
-}
-
-class Teacher implements TeacherInterface {
-  workFromHome(): string {
-    return 'Cannot work from home';
-  }
-
-  getCoffeeBreak(): string {
-    return 'Cannot have a break';
-  }
-
-  workTeacherTasks(): string {
-    return 'Getting to work';
-  }
-}
-
-function createEmployee(salary: number | string) {
-  if (typeof salary === 'number' && salary < 500) {
-    return new Teacher();
+function executeWork(employee: DirectorInterface | TeacherInterface): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
   } else {
-    return new Director();
+    return employee.workTeacherTasks();
   }
 }
